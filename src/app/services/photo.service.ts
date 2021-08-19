00dbff931653acbdefbe48ff1355afc3dcc09ab7
Victor Bravo Sánchez <vbravo@stulz.es>
2021-08-19 16:16:44 +0200

    Inicio camara

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Storage } from '@capacitor/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+
+  constructor() { }
+}
